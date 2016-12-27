42f8ebd63d6269dde915742a9be5372bc38b2c08
Joway Wang <joway.w@gmail.com>
2016-12-27 22:00:20 +0800

    add provoxy vagrant

---
+++ privoxy-ss.sh
@@ -0,0 +1,7 @@
+sudo apt-get install privoxy
+echo 'forward-socks5   /               10.0.2.2:1080 . '
+sudo /etc/init.d/privoxy restart
+echo 'export http_proxy=http://127.0.0.1:8118' >> /etc/profile
+echo 'export https_proxy=http://127.0.0.1:8118' >> /etc/profile
+source /etc/profile
+
