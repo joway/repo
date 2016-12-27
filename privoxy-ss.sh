053a1d95a2edb1f103b733af849a6e23be322002
Joway Wang <joway.w@gmail.com>
2016-12-27 22:07:21 +0800

    add -y

--- privoxy-ss.sh
+++ privoxy-ss.sh
@@ -1,4 +1,4 @@
-sudo apt-get install privoxy
+sudo apt-get install -y privoxy
 echo 'forward-socks5   /               10.0.2.2:1080 . '
 sudo /etc/init.d/privoxy restart
 echo 'export http_proxy=http://127.0.0.1:8118' >> /etc/profile
