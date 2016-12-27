a92a06ad3fecdc837224643d1b6b85a30ec991f7
Joway Wang <joway.w@gmail.com>
2016-12-27 23:11:50 +0800

    add alpine

---
+++ alpine-mirror.sh
@@ -0,0 +1,2 @@
+echo "http://mirrors.ustc.edu.cn/alpine/v3.3/main" > /etc/apk/repositories && \
+    echo "http://mirrors.ustc.edu.cn/alpine/v3.3/community" >> /etc/apk/repositories
--- privoxy-ss.sh
+++ privoxy-ss.sh
@@ -1,6 +1,6 @@
 sudo apt-get update
 sudo apt-get install -y privoxy
-echo 'forward-socks5   /               10.0.2.2:1080 . '
+echo 'forward-socks5   /               10.0.2.2:1080 . ' >> /etc/privoxy/config
 sudo /etc/init.d/privoxy restart
 echo 'export http_proxy=http://127.0.0.1:8118' >> /etc/profile
 echo 'export https_proxy=http://127.0.0.1:8118' >> /etc/profile
